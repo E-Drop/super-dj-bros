c4d953daeb935d98163feb91b768bdda488b6e5e
E-Drop <erosendo.developer@gmail.com>
2019-01-19 12:48:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+function main() {
+
+    var homeScreen =
+        `<div id="homeScreen">
+             <h1>HOMEEEEE</h1>
+             <div class="input">
+               <input type="text" placeholder="Player's Name"></input>
+             </div>
+             <div class="buttons">
+               <button id="empezar" class="button button-start">INSERT COIN</button>
+               <button class="button button-rules">RULES</button>
+             </div>
+             <div class="image">
+               <img src='./Images/Map.jpg'>
+             </div>
+          </div>`;
+    
+    var gameScreen =
+        `<div id="gameScreen">
+            <h1>JUEEEGO</h1>
+            <div class="buttons">
+                <button class="button button-rules">RULES</button>
+            </div>
+        </div>`;
+            
+    var gameOverScreen =
+        `<div id="gameScreen">
+            <h1>gameover</h1>
+            <div class="buttons">
+                <button id="empezar" class="button button-start">INSERT COIN</button>
+                <button class="button button-rules">RULES</button>
+            </div>
+        </div>`;
+            
+    var gameOver = function() {
+        buildNoGameScreen(gameOverScreen);
+    };
+    // Funcion que añade al elemento con id container el html que se le pasa en la variable del atributo
+    function buildScreen(html) {
+        var container = document.getElementById('container');
+        container.innerHTML = html;
+    } 
+    function buildNoGameScreen(screen) {
+        buildScreen(screen);
+        document.getElementById("empezar").addEventListener("click", jugar);
+    }
+    function jugar(){
+        buildScreen(gameScreen);
+        // var game = new Game();
+        setTimeout(gameOver, 3000);
+    }
+    
+    buildNoGameScreen(homeScreen);
+}
+
+window.addEventListener('load', main)
