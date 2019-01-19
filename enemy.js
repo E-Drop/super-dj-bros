a262081a66c0b0d7195fab55cfd59f73a0ad8cf3
E-Drop <erosendo.developer@gmail.com>
2019-01-19 17:22:59 +0100

    add movement to enemies

--- enemy.js
+++ enemy.js
@@ -4,11 +4,15 @@ function Enemy(canvas) {
     this.size = 50;
     this.y = (canvas.height)-150;
     this.x = canvas.width-100;
-    this.xSpeed = 5;
+    this.xSpeed = Math.floor((Math.random()*5)+2);
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
 }
 
 Enemy.prototype.draw = function() {
     this.ctx.fillRect(this.x, this.y, this.size, this.size);
+}
+
+Enemy.prototype.update = function() {
+    this.x -= this.xSpeed;
 }
--- game.js
+++ game.js
@@ -3,25 +3,36 @@
 function Game(canvas, gameEndHandler) {
     this.ctx = canvas.getContext('2d');
     this.player = new Player(canvas);
-    this.enemy = new Enemy(canvas);
+    this.enemy = [];
     this.platform = new Platform(canvas);
     this.gameEndHandler = gameEndHandler;
     this.loopId;
 }
 
-    Game.prototype.updateGame = function(){
+    Game.prototype.updateGame = function() {
+        // this.player.update();
+        this.createEnemy();
+        this.enemy.forEach(function(item) {
+            item.update();
+        });
+    }
 
+    Game.prototype.createEnemy = function() {
+        if (Math.random() > 0.99) {
+            this.enemy.push(new Enemy(canvas));
+        }
     }
 
     Game.prototype.clearCanvas = function(){
-
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
     Game.prototype.drawCanvas = function(){
+        this.enemy.forEach(function(item) {
+            item.draw();
+        });
         this.player.draw();
-        this.enemy.draw();
         this.platform.draw();
-
     }
 
     Game.prototype.gameEnd = function(){
@@ -30,9 +41,10 @@ function Game(canvas, gameEndHandler) {
 
     Game.prototype.start = function() {
         function loop() {
+            this.updateGame();
+            this.clearCanvas();
             this.drawCanvas();
             this.animation = window.requestAnimationFrame(loop.bind(this));
         }
-    
         this.animation = window.requestAnimationFrame(loop.bind(this));
     }
